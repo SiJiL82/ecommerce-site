0ee89a4fb157aa6f332aa2658000fa6b61fee18f
SiJiL82 <sven.lowry@gmail.com>
2022-07-27 12:13:44 +0100

    Disable collect static

--- ecommercesite/settings.py
+++ ecommercesite/settings.py
@@ -160,3 +160,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DISABLE_COLLECTSTATIC=1
