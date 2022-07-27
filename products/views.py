b3f5280db1f2b9c76a9222c094dc8c7a965a5aef
SiJiL82 <sven.lowry@gmail.com>
2022-07-27 16:50:26 +0100

    Add product page contents and styling

--- products/views.py
+++ products/views.py
@@ -7,7 +7,7 @@ def all_products(request):
     """ A view to show all products, including sorting and filtering """
     products = Product.objects.all()
     query = None
-    categories = None
+    selected_category = None
     sort = None
     direction = None
 
@@ -26,11 +26,12 @@ def all_products(request):
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
 
-
         if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+            category = request.GET['category']
+            print(category)
+            products = products.filter(category__name__in=(category,))
+            print(products)
+            selected_category = Category.objects.filter(name__in=(category, ))[0]
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -44,7 +45,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories,
+        'current_category': selected_category,
         'current_sorting': current_sorting
     }
 
