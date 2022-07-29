e60f9454cce534156858db5961da5d5fd6a5dbb3
SiJiL82 <sven.lowry@gmail.com>
2022-07-29 11:14:34 +0100

    Set delivery cost to static amount

--- checkout/models.py
+++ checkout/models.py
@@ -22,18 +22,19 @@ class Order(models.Model):
   grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
   def _generate_order_number(self):
+    """ Generate a unique order number """
     return uuid.uuid4().hex.upper()
 
   def update_total(self):
+    """ Update grand total each time a line item is added """
     self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total_sum']
-    if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-      self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
-    else:
-      self.delivery_cost = 0
+    self.delivery_cost = 3.20
+    
     self.grand_total = self.order_total + self.delivery_cost
     self.save()
 
   def save(self, *args, **kwargs):
+    """ Override the default save method to set an order number """
     if not self.order_number:
       self.order_number = self._generate_order_number()
     super().save(*args, **kwargs)
