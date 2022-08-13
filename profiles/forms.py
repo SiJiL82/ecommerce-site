f7e5b13c40e62c0dd2c2b9488f3ef377b146198b
SiJiL82 <sven.lowry@gmail.com>
2022-08-13 21:51:44 +0100

    Add default details form to profile

---
+++ profiles/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['default_phone_number'].label = 'Default Phone Number:'
+        self.fields['default_country'].label = 'Default Country:'
+        self.fields['default_postcode'].label = 'Default Postcode:'
+        self.fields['default_town_or_city'].label = 'Default Town or City:'
+        self.fields['default_street_address1'].label = 'Default\
+            Street Address 1:'
+        self.fields['default_street_address2'].label = 'Default\
+            Street Address 2:'
+        self.fields['default_county'].label = 'Default County:'
+
+        self.helper = FormHelper()
+        self.helper.form_show_labels = False
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
