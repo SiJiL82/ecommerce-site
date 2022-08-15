5f22c0f86773f89b349cb905b1d5e4e37e58b849
SiJiL82 <sven.lowry@gmail.com>
2022-08-15 19:47:31 +0100

    Add email config to settings

--- ecommercesite/settings.py
+++ ecommercesite/settings.py
@@ -35,8 +35,17 @@ ALLOWED_HOSTS = [env('HEROKU_APP_NAME'), '127.0.0.1']
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAUL = 'contact@minisewnsew.co.uk'
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'contact@minisewnsew.co.uk'
+else:
+    EMAIL_BACKED = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = env('EMAIL_HOST_USER')
 
 # Application definition
 
