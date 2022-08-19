1c3f9a460c6ccc1fa0b72eb60930439fdf04d485
SiJiL82 <sven.lowry@gmail.com>
2022-08-19 23:27:23 +0100

    Comment basket code

--- basket/contexts.py
+++ basket/contexts.py
@@ -5,13 +5,20 @@ from django.conf import settings
 
 
 def basket_contents(request):
+    """
+    Context processor to make basket information
+    available on all pages
+    """
 
+    # Initialise variables
     basket_items = []
     total = 0
     product_count = 0
     basket = request.session.get('basket', {})
 
+    # Loop through each item in the session basket and add to basket_items
     for item_id, item_data in basket.items():
+        # Check if it's an item with no size data
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
@@ -21,6 +28,7 @@ def basket_contents(request):
                 'quantity': item_data,
                 'product': product,
             })
+        # Items with size data
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
@@ -33,8 +41,10 @@ def basket_contents(request):
                     'size': size,
                 })
 
+    # Get delivery amount from settings
     delivery = Decimal(settings.DELIVERY_AMOUNT)
 
+    # Set grand total to order total + delivery amount
     grand_total = delivery + total
 
     context = {
--- basket/views.py
+++ basket/views.py
@@ -11,13 +11,17 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the basket """
 
+    # Init variables
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {})
     size = None
+
+    # Check if basket item has a size and set if applicable
     if 'product_size' in request.POST:
         size = request.POST['product_size']
-    basket = request.session.get('basket', {})
-
+    # If size is set, add to a unique key so duplicate items
+    # of different sizes aren't combined
     if size:
         if item_id in list(basket.keys()):
             if size in basket[item_id]['items_by_size'].keys():
@@ -32,7 +36,10 @@ def add_to_basket(request, item_id):
         else:
             basket[item_id] = quantity
 
+    # Set the session basket to the updated basket object
     request.session['basket'] = basket
+
+    # Show success message to user
     message = """
         Item added to
         <a href='{url}' class='text-brand'>
@@ -44,18 +51,23 @@ def add_to_basket(request, item_id):
         request,
         mark_safe(message.format(url=basket_url))
     )
+
     return redirect(redirect_url)
 
 
 def adjust_basket(request, item_id):
     """ Adjust quantity of the specified product to the specified amount """
 
+    # Init variables
     quantity = int(request.POST.get('quantity'))
     size = None
+
+    # Check if product has size property
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     basket = request.session.get('basket', {})
 
+    # If size is set, make sure we adjust the correct basket item
     if size:
         if quantity > 0:
             basket[item_id]['items_by_size'][size] = quantity
@@ -69,19 +81,25 @@ def adjust_basket(request, item_id):
         else:
             basket.pop(item_id)
 
+    # Update the session basket to the basket object
     request.session['basket'] = basket
+
     return redirect(reverse('view_basket'))
 
 
 def remove_from_basket(request, item_id):
-    """Remove the item from the shopping basket"""
+    """ Remove the item from the shopping basket """
 
     try:
+        # Init variables
         size = None
+
+        # Check if item has size property
         if 'product_size' in request.POST:
             size = request.POST['product_size']
         basket = request.session.get('basket', {})
 
+        # If size is set, make sure we remove the correct item from the basket.
         if size:
             del basket[item_id]['items_by_size'][size]
             if not basket[item_id]['items_by_size']:
@@ -89,11 +107,18 @@ def remove_from_basket(request, item_id):
         else:
             basket.pop(item_id)
 
+        # Update the session basket to the basket object
         request.session['basket'] = basket
+
+        # Show success message to the user
         messages.success(
             request,
             'Item removed from basket'
         )
+
         return HttpResponse(status=200)
     except Exception:
+        # Return an error as product can't be removed from the basket.
+        # TODO: This should really invalidate the user's whole basket
+        # as something has gone very wrong
         return HttpResponse(status=500)
